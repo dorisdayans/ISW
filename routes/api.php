7249568210652ad5372b45ada4a8c3c8cf40ef3a
dayans <dorisdayans@gmail.com>
2026-03-23 21:23:59 -0600

    rutas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\EnrollmentController;
 
 //Route::apiResource('courses', CourseController::class);
 
@@ -24,5 +26,12 @@ Route::apiResource('courses', CourseController::class)
     ->missing(function (Request $request) {
         return response()->json([
             'message' => 'Curso no encontrado.',
+        ], 404);
+    });
+
+Route::apiResource('enrollments', EnrollmentController::class)
+    ->missing(function (Request $request) {
+        return response()->json([
+            'message' => 'Matrícula no encontrada.',
         ], 404);
     });
